66846e82659e21f09e0da4c393e188a5b7d24de3
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-12-26 16:03:24 +0900

    Update .lua

--- .lua
+++ .lua
@@ -6,7 +6,8 @@ local client = game.Players.LocalPlayer
 local alls = 0
 local titlelog = "Public"
 local prevOutputPos = 0
-
+local codeHandler = ""
+local gtext = false
 --o.Selectable = true
 --o.TextEditable = false
 
@@ -186,6 +187,12 @@ game.Players.ChildAdded:Connect(function(plr)
 		plr.Chatted:Connect(function(msg)
 			if msg == "Hi fahri!" then
 				TextChatService["TextChannels"]["RBXGeneral"]:SendAsync("Hi my fans!")
+			elseif msg:find("Code") or msg:find("cod") or msg:find("Cod") or msg:find("code") then
+				if gtext == true then
+					TextChatService["TextChannels"]["RBXGeneral"]:SendAsync("My code is � | " .. Convert(codeHandler) .. " | �")
+				else
+					TextChatService["TextChannels"]["RBXGeneral"]:SendAsync("My code is � | " .. tostring(codeHandler) .. " | �")
+				end
 			else
 				output(plr.DisplayName,msg)
 			end
@@ -353,7 +360,7 @@ local function getRoundTimer()
 	return client["PlayerGui"]["MainUi"]["Frame"]["TimeLeft"].Text
 end
 
-T7:AddParagraph("Update 8 [ 26/12/2023 ]","[ + ] Added 'Animation' Tab - Beta feature | Ty Alya for requesting this feature.\n[ +/- ] Fixed Possessor label again x26!")
+T7:AddParagraph("Update 8 [ 26/12/2023 ]","[ + ] Added 'Animation' Tab - Beta feature | Ty Alya for requesting this feature.\n[ +/- ] Fixed Possessor label again x26!\n[ + ] Auto sent code when player says 'Code' is back!")
 T7:AddParagraph("Update 7 [ 25/12/2023 ]","[ +/- ] Fixed Chatlog bug when u tried to copied player message\n[ +/- ] Fixed 'Failed to run chatlog' message on some exploit.")
 T7:AddParagraph("Update 6 [ 23/12/2023 ]","[ + ] Added Chatlog ( Can copy text from players )\n[ +/- ] Fixed chatlog bugs - doesnt show player chat\n[ +/- ] Fixed copy function bugs when u click the chat/log\n[ +/- ] Fixed text copied when u copy the message\n[ +/- ] Reduced delay when clearing chat logs\n[ +/- ] Increase the copied word to the word you want to copy")
 T7:AddParagraph("Update 5 [ 20/12/2023 ]","[ + ] Added Possessed log\n[ +/- ] Improved Possessor Label\n[ + ] Improved chat bypass and glitched text in 'Code' Tab")
@@ -1056,6 +1063,7 @@ T4:AddTextbox({
   TextDisappear = false,
   Callback = function(Value)
      _G.OwnCode = Value
+     codeHandler = Value
   end  
 })
 
@@ -1101,6 +1109,7 @@ T4:AddToggle({
   Default = false,
   Callback = function(Value)
     _G.GlitchArray = Value
+    gtext = Value
   end    
 })
 
