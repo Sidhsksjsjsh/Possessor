cb32570eb0c9dc656d4edf71de0f09a57562899b
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-01-12 16:42:54 +0900

    Update .lua

--- .lua
+++ .lua
@@ -468,7 +468,7 @@ for i,v in pairs(game:GetService("Players"):GetPlayers()) do
         TextLabel.BackgroundTransparency = 1
         TextLabel.Size = UDim2.new(1, 0, 1, 0)
         TextLabel.Text = v.DisplayName .. "\n<font color='rgb(255,0,0)'>@" .. v.Name .. "</font>"
-        TextLabel.TextColor3 = Color3.new(1,0,0)
+        TextLabel.TextColor3 = Color3.new(1,1,0)
         TextLabel.TextScaled = false
         TextLabel.Name = "UI 1"
 	
